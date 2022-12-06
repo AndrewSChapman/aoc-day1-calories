a05233b4c694876d1b81e9744d8a5578822f89f5
Andrew Chapman <andrew.chapman@team.bumble.com>
2022-12-06 09:36:11 +0000

    Added day 6

---
+++ day6/challenge-2.php
@@ -0,0 +1,32 @@
+<?php
+$input = file_get_contents('input.txt');
+//$input = 'bvwbjplbgvbhsrlpgdmjqwftvncz';
+
+$signalStart = getSignalStartPosition($input);
+
+print "Signal start: $signalStart\n";
+
+
+function getSignalStartPosition(string $input): int
+{
+    $inputLen = strlen($input);
+    $headerLength = 14;
+
+    for ($offset = 0; $offset <= ($inputLen - $headerLength); $offset++) {
+        $possibleHeader = substr($input, $offset, $headerLength);
+        if (allCharsAreUnique($possibleHeader)) {
+            return $offset + $headerLength;
+        }
+    }
+
+    throw new Exception('Valid header not found');
+}
+
+function allCharsAreUnique(string $header): bool
+{
+    $headerLen = strlen($header);
+    $headerElements = str_split($header);
+    $uniqueElements = array_unique($headerElements);
+
+    return count($uniqueElements) === $headerLen;
+}
